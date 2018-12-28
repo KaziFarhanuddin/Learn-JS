1abcc36f05d0d63b7c926b1b469a7a17f101bd27
kazifarhanuddin <kazifarhan199@gmail.com>
2018-12-29 01:59:18 +0530

    Added Basics 01

---
+++ 01 Basics/celcious.js
@@ -0,0 +1,9 @@
+var score = 100
+score = 130 
+const bonus = 20
+var finalScore = score + bonus
+// console.log(finalScore)
+
+let tempFarinite = 100
+let tempCelsius = (tempFarinite-32) * 5/9
+console.log(tempCelsius)
---
+++ 01 Basics/nullBool.js
@@ -0,0 +1,11 @@
+let a = null
+
+// console.log(a)
+
+let marks = 100
+
+let pass = (marks>40)
+
+if (pass){
+    console.log('You pass')
+}
